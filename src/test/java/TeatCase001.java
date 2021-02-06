33a34407b6b864f8817b7f903ac5c36ef2838fb6
MehmetEminYekta <mhmtmn.14zg@gmail.com>
2021-02-06 17:19:16 +0100

    completed the tc001 with few utility and some assertions

--- src/test/java/TeatCase001.java
+++ src/test/java/TeatCase001.java
@@ -1,8 +1,10 @@
 public class TeatCase001 {
 
 
-    //I am about to write some awesome code for this test case
+    // I am about to write some awesome code for this test case
 
     // writing some awesome method to reverse a string
+
+    // completed this awesome test cases ready to merge
 
 }
